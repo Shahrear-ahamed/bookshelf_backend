c15ad2e94137577efc69b1b5da807692ff894729
Shahrear-ahamed <its.shahrear@gmail.com>
2023-07-14 23:20:44 +0600

    [DONE] set router and route not found error fix

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import express, { Application } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
+import httpStatus from 'http-status'
+import routers from './app/routes'
 
 const app: Application = express()
 
@@ -13,7 +15,24 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // routes
+app.use('/api/v1', routers)
 
 // global error handler
+
+// not found handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    status: 'failed',
+    message: req.originalUrl + ' Url not found',
+    errorMessage: [
+      {
+        path: req.originalUrl,
+        message: req.originalUrl + ' Url not found',
+      },
+    ],
+  })
+
+  next()
+})
 
 export default app
---
+++ src/app/routes/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const router = express.Router()
+
+// const routerModules = [
+//   {
+//     path: '/auth',
+//     route: 'authRoute',
+//   },
+// ]
+
+// routerModules.forEach(module => {
+//   router.use(module.path, module.route)
+// })
+
+export default router
